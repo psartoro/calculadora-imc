1b12e3dc1ae97f68dd39d9f175e2ebab46a304b2
Pedro Sartoro <46354131+psartoro@users.noreply.github.com>
2021-02-25 09:51:32 -0300

    Melhorias
    Inserido validações de valores vazio e não numéricos

--- exCalculadoraImc.py
+++ exCalculadoraImc.py
@@ -1,44 +1,60 @@
-print('====CALCULADORA IMC====')
-nome = bool(input('Insira seu nome: '))
+print('{:-^25}\n'.format('CALCULADORA IMC'))
+nome = ""
+peso = ""
+altura = ""
 
 
 # Funcao
 # Se um dos valores for igual a vazio, retorna 0
 def calculaimc(pes, alt):
-    if pes != '' and alt != '':
+    try:
         pes = float(pes)
         alt = float(alt)
         imc = pes / (alt * alt)
         return imc
-    else:
+    except:
         return 0
 
 
-if not nome:
-    print('Fim... valores inseridos inválidos!!!')
+while not nome or not peso or not altura:
+    nome = input('Digite seu nome: ').strip()
+    peso = input('Digite seu peso: ').strip()
+    altura = input('Digite sua altura: ').strip()
+    dados = [nome, peso, altura]
+    for n in range(3):
+        if dados[n] == '':
+            print('Valores incorretos!')
+
+imc = calculaimc(dados[1], dados[2])
+
+while imc == 0:
+    print('Peso e Altura devem ser numéricos!')
+    print('-' * 45)
+    peso = input('Digite seu peso: ').strip()
+    altura = input('Digite sua altura: ').strip()
+    dados[1] = peso
+    dados[2] = altura
+    imc = calculaimc(dados[1], dados[2])
+
+print('-'*45)
+
+if imc < 18.5:
+    print('Seu IMC é de:', round(imc))
+    print('Cuidado {}!!! Você está abaixo do peso!'.format(nome.capitalize()))
+elif 18.5 <= imc <= 24.9:
+    print('Seu IMC é de:', round(imc))
+    print('Parabéns {}!!! Seu peso está normal!'.format(nome.capitalize()))
+elif 25.0 <= imc <= 29.9:
+    print('Seu IMC é:', round(imc))
+    print('Cuidado {}!!! Você está acima de seu peso!'.format(nome.capitalize()))
+elif 30.0 <= imc <= 34.9:
+    print('Seu IMC é:', round(imc))
+    print('Atenção {}!!! Obesidade grau I'.format(nome.capitalize()))
+elif imc >= 35.0:
+    print('Seu IMC é:', round(imc))
+    print('Atenção {}!!! Obesidade grau II'.format(nome.capitalize()))
 else:
-    peso = input('Insira seu peso: ')
-    altura = input('Insira sua altura: ')
-    # Chamada da Funcao
-    imc = calculaimc(peso, altura)
-    if imc > 0:
-        if imc < 18.5:
-            print('Seu IMC é de:', round(imc))
-            print('Cuidado {}!!! Você está abaixo do peso!'.format(nome))
-        elif 18.5 <= imc <= 24.9:
-            print('Seu IMC é de:', round(imc))
-            print('Parabéns {}!!! Seu peso está normal!'.format(nome))
-        elif 25.0 <= imc <= 29.9:
-            print('Seu IMC é:', round(imc))
-            print('Cuidado {}!!! Você está acima de seu peso!'.format(nome))
-        elif 30.0 <= imc <= 34.9:
-            print('Seu IMC é:', round(imc))
-            print('Atenção {}!!! Obesidade grau I'.format(nome))
-        elif imc >= 35.0:
-            print('Seu IMC é:', round(imc))
-            print('Atenção {}!!! Obesidade grau II'.format(nome))
-        else:
-            print('Seu IMC é:', round(imc))
-            print('Atenção {}!!! Obesidade grau III'.format(nome))
-    else:
-        print('Fim... valores inseridos inválidos!!!')
+    print('Seu IMC é:', round(imc))
+    print('Atenção {}!!! Obesidade grau III'.format(nome.capitalize()))
+
+print('-'*45)
