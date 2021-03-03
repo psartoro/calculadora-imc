53287ca89d47e814c7e3e757858fe598f99c9932
Pedro Sartoro <46354131+psartoro@users.noreply.github.com>
2021-03-03 17:57:24 -0300

    Add files via upload

--- exCalculadoraImc.py
+++ exCalculadoraImc.py
@@ -3,40 +3,20 @@ nome = ""
 peso = ""
 altura = ""
 
-
-# funcao
-def calculaimc(pes, alt):
-    try:
-        pes = float(pes)
-        alt = float(alt)
-        imc = pes / (alt * alt)
-        return imc
-    except:
-        return 0
-
-
-while not nome or not peso or not altura:
+while True:
     nome = input('Digite seu nome: ').strip()
     peso = input('Digite seu peso: ').strip()
     altura = input('Digite sua altura: ').strip()
     dados = [nome, peso, altura]
-    for n in range(3):
-        if dados[n] == '':
-            print('Valores incorretos!')
-            break
+    if '0' in dados or '' in dados:
+        print('Valores incorretos!')
+    else:
+        break
 
-imc = calculaimc(dados[1], dados[2])
+calc = lambda x, y: x / (y ** 2)
+imc = calc(float(dados[1]), float(dados[2]))
 
-while imc == 0:
-    print('Peso e Altura devem ser numéricos!')
-    print('-' * 45)
-    peso = input('Digite seu peso: ').strip()
-    altura = input('Digite sua altura: ').strip()
-    dados[1] = peso
-    dados[2] = altura
-    imc = calculaimc(dados[1], dados[2])
-
-print('-'*45)
+print('-' * 45)
 
 if imc < 18.5:
     print('Seu IMC é de:', round(imc))
@@ -57,4 +37,4 @@ elif 40 <= imc:
     print('Seu IMC é:', round(imc))
     print('\033[1;31mAtenção {}!!!\033[m Obesidade grau III'.format(nome.capitalize()))
 
-print('-'*45)
+print('-' * 45)
